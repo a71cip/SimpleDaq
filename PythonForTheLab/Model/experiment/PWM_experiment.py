633154ac81dfb3a949087189d9e29fc4a341d18f
a71cip <a71cip@tiscali.it>
2020-02-23 22:55:08 +0100

    experimento

--- PythonForTheLab/Model/experiment/PWM_experiment.py
+++ PythonForTheLab/Model/experiment/PWM_experiment.py
@@ -54,7 +54,43 @@ class Experiment:
         print('Pwm step : %s' %(step))
         print('Pwm delay : %s' %(delay))
 
+
+    def load_daq(self, daq_model=None):
+        """ Loads a DAQ Model already initialized or loads from yaml specifications. The DAQ that can
+        be provided through the YAML are 'DummyDaq' and 'RealDaq'. There are no limitations regarding
+        an already initialized DAQ provided that follows the Daq Model.
+        :param daq_model: it can be a model already initailized. If not provided, loads the default.
+        """
+
+        if daq_model is None:
+            if 'DAQ' in self.properties:
+                name = self.properties['DAQ']['name']
+                port = self.properties['DAQ']['port']
+
+                if name == 'DummyDaq':
+                    from PythonForTheLab.Model.daq import DummyDaq
+                    self.daq = DummyDaq(port)
+
+                elif name == 'Nano':
+                    print('NANO')
+                    #from ..daq.analog_daq_NANO import AnalogDaq
+                    #self.daq = AnalogDaq(port)
+
+                elif name == 'PyVisa':
+                    #from PythonForTheLab.Model.daq.visa_daq import AnalogDaq
+                    #self.daq = AnalogDaq(port)
+                    pass
+                else:
+                    filename = self.properties['config_file']
+                    raise Exception('The daq specified in {} does not exist in this program'.format(filename))
+            else:
+                filename = self.properties['config_file']
+                raise Exception("No DAQ specified in {}".format(filename))
+        else:
+            self.daq = daq_model
+
+
 if __name__ == "__main__":
     e = Experiment()
     e.load_config('experiment.yml')
-    #e.load_daq()
+    e.load_daq()
